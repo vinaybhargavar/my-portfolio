d5208cff6f066376776f87e7ef6d52bccb352c12
Vinay Bhargav Arni Ragunathan <55503680+vinaybhargavar@users.noreply.github.com>
2020-03-10 16:30:44 -0500

    Add routes to get to a particular project using Project Slug (#3)

--- routes/main.js
+++ routes/main.js
@@ -18,7 +18,26 @@ router.get('/', (req, res) => {
   .catch(err => {
     res.send('Oops! ' + err.message)
   })
+})
 
+router.get('/project/:slug', (req, res) => {
+  const data = req.context
+  const projectSlug = req.params.slug
+  const projectCtr = new ProjectController()
+  projectCtr.get({slugs:projectSlug})
+  .then(projects => {
+    if(projects.length == 0 ){
+      throw new Error('Project not found')
+      return
+    }
+    
+    const project = projects[0]
+    data['projects'] = project
+    res.render('project', data)
+  })
+  .catch(err => {
+    res.send('Oops! ' + err.message)
+  })
 })
 
 module.exports = router
